2e44f9227d276c55a470a956251eceff6d1f99bf
David Churchill <dave.churchill@gmail.com>
2017-08-14 22:33:15 -0230

    techtree

--- src/TechTree.h
+++ src/TechTree.h
@@ -7,12 +7,16 @@ class BuildType;
 
 struct TechTreeItem
 {
-    sc2::Race                    m_race;                // the race of this item
-    sc2::AbilityID               m_buildAbility;        // the ability that creates this item
-    sc2::AbilityID               m_warpAbility;         // the ability that creates this item via warp-in
-    std::vector<sc2::UnitTypeID> m_whatBuilds;          // any of these units can build the item
-    std::vector<sc2::UnitTypeID> m_requiredUnits;       // owning ONE of these is required to make
-    std::vector<sc2::UpgradeID>  m_requiredUpgrades;    // having ALL of these is required to make
+    sc2::Race                       race;             // the race of this item
+    int                             mineralCost;      // mineral cost of the item
+    int                             gasCost;          // gas cost of the item
+    int                             supplyCost;       // supply cost of the item
+    int                             buildTime;        // build time of the item
+    sc2::AbilityID                  buildAbility;     // the ability that creates this item
+    sc2::AbilityID                  warpAbility;      // the ability that creates this item via warp-in
+    std::vector<sc2::UnitTypeID>    whatBuilds;       // any of these units can build the item
+    std::vector<sc2::UnitTypeID>    requiredUnits;    // owning ONE of these is required to make
+    std::vector<sc2::UpgradeID>     requiredUpgrades; // having ALL of these is required to make
 };
 
 class TechTree
@@ -28,21 +32,7 @@ public:
 
     TechTree(CCBot & bot);
 
-    const std::vector<sc2::UnitTypeID> &    getWhatBuilds(const sc2::UnitTypeID & type) const;
-    const std::vector<sc2::UnitTypeID> &    getRequiredUnits(const sc2::UnitTypeID & type) const;
-    const std::vector<sc2::UpgradeID> &     getRequiredUpgrades(const sc2::UnitTypeID & type) const;
-    const sc2::AbilityID &                  getBuildAbility(const sc2::UnitTypeID & type) const;
-    const sc2::AbilityID &                  getWarpAbility(const sc2::UnitTypeID & type) const;
-
-    const std::vector<sc2::UnitTypeID> &    getWhatBuilds(const sc2::UpgradeID & type) const;
-    const std::vector<sc2::UnitTypeID> &    getRequiredUnits(const sc2::UpgradeID & type) const;
-    const std::vector<sc2::UpgradeID> &     getRequiredUpgrades(const sc2::UpgradeID & type) const;
-    const sc2::AbilityID &                  getBuildAbility(const sc2::UpgradeID & type) const;
-    const sc2::AbilityID &                  getWarpAbility(const sc2::UpgradeID & type) const;
-
-    const std::vector<sc2::UnitTypeID> &    getWhatBuilds(const BuildType & type) const;
-    const std::vector<sc2::UnitTypeID> &    getRequiredUnits(const BuildType & type) const;
-    const std::vector<sc2::UpgradeID> &     getRequiredUpgrades(const BuildType & type) const;
-    const sc2::AbilityID &                  getBuildAbility(const BuildType & type) const;
-    const sc2::AbilityID &                  getWarpAbility(const BuildType & type) const;
+    const TechTreeItem & getData(const sc2::UnitTypeID & type) const;
+    const TechTreeItem & getData(const sc2::UpgradeID & type)  const;
+    const TechTreeItem & getData(const BuildType & type)       const;
 };
