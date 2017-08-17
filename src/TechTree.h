2b05029ce2e0f221a7afa75cbb914d49444ea32d
David Churchill <dave.churchill@gmail.com>
2017-08-17 17:28:20 -0230

    techtree

--- src/TechTree.h
+++ src/TechTree.h
@@ -7,20 +7,20 @@ class BuildType;
 
 struct TypeData
 {
-    sc2::Race                       race;             // the race of this item
-    int                             mineralCost;      // mineral cost of the item
-    int                             gasCost;          // gas cost of the item
-    int                             supplyCost;       // supply cost of the item
-    int                             buildTime;        // build time of the item
-    bool                            isUnit;
-    bool                            isBuilding;
-    bool                            isWorker;
-    bool                            isRefinery;
-    bool                            isSupplyDepot;
-    bool                            isTownHall;
-    bool                            isAddon;
-    sc2::AbilityID                  buildAbility;     // the ability that creates this item
-    sc2::AbilityID                  warpAbility;      // the ability that creates this item via warp-in
+    sc2::Race                       race            = sc2::Race::Random;             // the race of this item
+    int                             mineralCost     = 0;      // mineral cost of the item
+    int                             gasCost         = 0;          // gas cost of the item
+    int                             supplyCost      = 0;       // supply cost of the item
+    int                             buildTime       = 0;        // build time of the item
+    bool                            isUnit          = false;
+    bool                            isBuilding      = false;
+    bool                            isWorker        = false;
+    bool                            isRefinery      = false;
+    bool                            isSupplyDepot   = false;
+    bool                            isTownHall      = false;
+    bool                            isAddon         = false;
+    sc2::AbilityID                  buildAbility    = 0;     // the ability that creates this item
+    sc2::AbilityID                  warpAbility     = 0;      // the ability that creates this item via warp-in
     std::vector<sc2::UnitTypeID>    whatBuilds;       // any of these units can build the item
     std::vector<sc2::UnitTypeID>    requiredUnits;    // owning ONE of these is required to make
     std::vector<sc2::UpgradeID>     requiredUpgrades; // having ALL of these is required to make
