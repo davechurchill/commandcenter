054eb23d3d6c2a4bede58126ca90ebc85b4fc344
Lucas Critch <lrc374@mun.ca>
2018-07-06 16:11:37 -0230

    added 2 new mineral unit types, & optional numbers in build order (botconfig.txt - ex: "SCV" or "# SCV")

--- src/StrategyManager.cpp
+++ src/StrategyManager.cpp
@@ -163,22 +163,61 @@ void StrategyManager::readStrategyFile(const std::string & filename)
                 BOT_ASSERT(val.count("Race") && val["Race"].is_string(), "Strategy is missing a Race string");
                 CCRace strategyRace = Util::GetRaceFromString(val["Race"].get<std::string>());
                 
-                BOT_ASSERT(val.count("OpeningBuildOrder") && val["OpeningBuildOrder"].is_array(), "Strategy is missing an OpeningBuildOrder arrau");
-                BuildOrder buildOrder;
-                const json & build = val["OpeningBuildOrder"];
-                for (size_t b(0); b < build.size(); b++)
-                {
-                    if (build[b].is_string())
-                    {
-                        MetaType MetaType(build[b].get<std::string>(), m_bot);
-                        buildOrder.add(MetaType);
-                    }
-                    else
-                    {
-                        BOT_ASSERT(false, "Build order item must be a string %s", name.c_str());
-                        continue;
-                    }
-                }
+				BOT_ASSERT(val.count("OpeningBuildOrder") && val["OpeningBuildOrder"].is_array(), "Strategy is missing an OpeningBuildOrder array");
+				BuildOrder buildOrder;
+				const json & build = val["OpeningBuildOrder"];
+				for (size_t b(0); b < build.size(); b++)
+				{
+					if (build[b].is_string())
+					{
+						// this is the raw string for the build order item in the config file
+						const std::string & buildItem(build[b]);
+
+						size_t numitems = 1;
+						std::string itemName;
+						std::stringstream ss(buildItem);
+						bool startsWithNumber = isdigit(buildItem[0]); // first char digit or not
+
+						for (size_t i = 0; i < buildItem.length(); i++)
+						{
+							if (buildItem[i] == ' ') break;
+
+							if (!isdigit(buildItem[i]))
+							{
+								startsWithNumber = false;
+								break;
+							}
+						}
+
+						if (startsWithNumber)
+						{
+							// numitems and then itemName because they are space separated and format is "5 SCV" 
+							// can change to different delimiter if needed
+							ss >> numitems;
+							ss >> itemName;
+						}
+
+						else
+						{
+							// no number here so format is "SCV"
+							ss >> itemName;
+						}
+
+						// Construct the metatype based on the extracted type name
+						MetaType metaType(itemName, m_bot);
+						// Add the meta type numTimes to the build order	
+						for (size_t i(0); i < numitems; i++)
+						{
+							buildOrder.add(metaType);
+						}
+					}
+
+					else
+					{
+						BOT_ASSERT(false, "Build order item must be a string %s", name.c_str());
+						continue;
+					}
+				}
 
                 BOT_ASSERT(val.count("ScoutCondition") && val["ScoutCondition"].is_array(), "Strategy is missing a ScoutCondition array");
                 Condition scoutCondition(val["ScoutCondition"], m_bot);
